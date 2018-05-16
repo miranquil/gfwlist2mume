d9784cce15a3734637ad9e70b2e260b88e5c50e5
Yorushika <yorushika@outlook.com>
2018-05-16 09:41:28 +0800

    Adjust source code, complete readme.

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # gfwlist2mume
-A script transforms gfwlist to rules which can be imported into Mume&amp;寒梅 at iOS.
 
+[![GenPAC](https://img.shields.io/badge/workwith-genpac-brightgreen.svg)](https://github.com/JinnLynn/GenPAC) [![License](https://img.shields.io/badge/license-GPL--3.0-brightgreen.svg)](https://github.com/yorushika/gfwlist2mume/blob/master/LICENSE)
 
+A script transforms gfwlist to rules which can be imported directly to Mume&amp;寒梅 at iOS.
 
 在线获取GFWList并转换为寒梅&Mume规则的脚本，可直接用于App内URL导入功能。
 
+## Deploy
 
+---
 
-## 部署
+### 使用Docker
 
-###  使用Docker
+#### 本地构建
 
-#### 你需要部署在一台服务器上用来导入。
-
-```docker build -t gfwlist2mume .
+```docker
 docker build -t gfwlist2mume .
 docker run --rm -it -p 8000:8000 gfwlist2mume
 ```
 
+#### Docker Hub
+
+```docker
+docker pull yorushika/gfwlist2mume
+docker run --rm -it -p 8000:8000 yorushika/gfwlist2mume
+```
--- main.py
+++ main.py
@@ -10,24 +10,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    data = {
-        'name': 'sjn',
-        'type': 'kkk',
-    }
     return json.dumps(data_generate())
 
 
 def data_generate():
     gfwlist_url = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
-    gfwlist_txt = urllib.request.urlopen(gfwlist_url).read()
-    gfwlist_txt = gfwlist_txt.decode()
+    gfwlist_txt = urllib.request.urlopen(gfwlist_url).read().decode()
+
     with open('gfwlist.txt', 'w', encoding='utf-8') as fout:
         for line in gfwlist_txt:
             fout.write(line)
+
     os.system(
         'genpac --format=pac --gfwlist-url - --gfwlist-local gfwlist.txt --pac-proxy="SOCKS5 127.0.0.1:8080" -o pac.txt')
+
     with open("pac.txt", "r", encoding='utf-8') as pacfile:
         pac_data = pacfile.read()
+        
     rule_data = re.search(r'var rules = ([\S\s]*?]);', pac_data).group(1)
     rule_data = json.loads(rule_data)
     domains = rule_data[1][1]
@@ -42,15 +41,6 @@ def data_generate():
         }
         rules.append(item)
 
-    # data_dict = {
-    #     'id': '596f7275-7368-696b-6100-6f776e706163',
-    #     'enabled': True,
-    #     'updated_at': time.strftime("%Y-%m-%d", time.localtime()),
-    #     "rules": rules,
-    #     'created_at': time.strftime("%Y-%m-%d", time.localtime()),
-    #     'is_official': False,
-    #     'name': 'GFWList',
-    #     'description': '由GFWList生成的规则'
     rule_set = {
         'is_official': False,
         'description': '由GFWList生成的规则',
@@ -58,7 +48,6 @@ def data_generate():
         "rules": rules,
         'name': 'GFWList',
     }
-    # }
 
     rule_set_list = [rule_set]
     result = {
